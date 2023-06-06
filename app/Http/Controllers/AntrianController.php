1ac39387dad6bd363074ce47dba85a2565de682b
marwandhiaurrahman <marwandhiaurrahman@gmail.com>
2023-06-06 12:23:34 +0700

    api poliklinik aktif

--- app/Http/Controllers/AntrianController.php
+++ app/Http/Controllers/AntrianController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class AntrianController extends Controller
 {
     public function daftar(Request $request)
     {
+        $url = "http://sim.rsudwaled.id/siramah/api/poliklinik_all";
+        $res = Http::get($url);
         return view('daftar');
     }
 }
